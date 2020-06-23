266d5608331c105bceb19eef49c6f71e86bfdb12
habib <bellaaj.habib@gmail.com>
2020-06-23 08:06:18 +0200

    drag and drop ts modules
    Signed-off-by: habib <bellaaj.habib@gmail.com>

---
+++ src/models/drag-drop.ts
@@ -0,0 +1,17 @@
+namespace App {
+    // Drag & Drop Interfaces
+    export interface Draggable {
+        dragStartHandler(event: DragEvent): void;
+
+        dragEndHandler(event: DragEvent): void;
+    }
+
+    export interface DragTarget {
+        dragOverHandler(event: DragEvent): void;
+
+        dropHandler(event: DragEvent): void;
+
+        dragLeaveHandler(event: DragEvent): void;
+    }
+}
+
