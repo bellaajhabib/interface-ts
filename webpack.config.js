2cb6c7a91c102dc33bc95e54b3b090e9f7db9b62
habib <bellaaj.habib@gmail.com>
2020-06-30 23:48:00 +0200

    webPackMaps
    Signed-off-by: habib <bellaaj.habib@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.ts',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /nodeModules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    }
+  mode: 'development',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
 };
